c9a6a5fed69e3479ecc9314362f31419b6f338e7
Shoko Sugito <fruitmachine0@gmail.com>
2019-07-28 16:36:37 +0900

    アップロード中はアップロードできない

--- app/components/fileUploader/index.vue
+++ app/components/fileUploader/index.vue
@@ -3,11 +3,14 @@ el-form-item
   label.m-fileuploader(
   @dragover.stop.prevent='handleDragover'
   @drop.stop.prevent='uploadFilesFromDnD'
+  :disabled='uploading'
   )
-    | ドラッグ&ドロップで画像を選択
+    i.mr-5(:class='label.icon')
+    | {{ label.text }}
     input(
     type='file'
     multiple
+    :disabled='uploading'
     @change='uploadFilesFromInput'
     )
 </template>
@@ -16,14 +19,32 @@ el-form-item
 import { upload } from '~/utils/firebase'
 import { getRandomString } from '~/utils/string'
 export default {
+  data() {
+    return {
+      uploading: false
+    }
+  },
+  computed: {
+    label() {
+      return this.uploading
+        ? { icon: 'el-icon-loading', text: 'アップロード中' }
+        : {
+            icon: 'el-icon-upload2',
+            text: 'ドラッグ&ドロップで画像をアップロード'
+          }
+    }
+  },
   methods: {
-    uploadFile(files) {
+    async uploadFile(files) {
+      this.uploading = true
       for (let i = 0; i < files.length; i++) {
         const filname = getRandomString()
         const ext = files[i].name.match(/\.[a-z]*$/i)
         try {
-          upload({ path: '/images/' + filname + ext, file: files[i] })
+          await upload({ path: '/images/' + filname + ext, file: files[i] })
+          this.uploading = false
         } catch (error) {
+          this.uploading = false
           throw error
         }
       }
